cb96bf400d0507c0abe6534c5755f0adf1ff50fd
ErykDarnowski <github@erykdarnowski.pl>
2022-10-19 18:17:38 +0200

    Update preview launcher script

--- prev.sh
+++ prev.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 clear='\033[0m'
+
 function color_red () {
     echo -ne '\033[31m'"$1"$clear
 }
-
 function is_installed () {
 	return $(which $1 > /dev/null 2>&1)
 }
 
 
+# Clear terminal
 printf "\033c"
 
-if is_installed 'grip'; then
-	grip
+if is_installed 'nvim'; then
+	nvim README.md +'CocCommand markdown-preview-enhanced.openPreview'
 	exit 0
 else
-	echo -e "$(color_red "Couldn't find \`grip\`!!!\nInstall it please:") https://github.com/joeyespo/grip#installation"
+	echo -e "$(color_red "Couldn't find \`neovim\`!!!\nPlease install both it and this extension:") https://github.com/weirongxu/coc-markdown-preview-enhanced"
 	exit 1
 fi
